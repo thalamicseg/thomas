bc7f28b55e340e423691692e432f9f02c39c02bd
Jason Su <git@jason.su>
2020-02-09 19:29:14 -0800

    Added argparse and control for number of processses

--- form_multiatlas.py
+++ form_multiatlas.py
@@ -9,6 +9,7 @@ import sys
 import re
 import shelve
 import tempfile
+import argparse
 from collections import OrderedDict
 from libraries.parallel import BetterPool, command
 
@@ -92,17 +93,21 @@ class CompareOverlap(object):
 #     'Fixed_Metric': ['2-AV', '6-VLP', '4-VA', '5-VLa', '8-Pul', '7-VPL', '9-LGN', '10-MGN', '12-MD-Pf', '11-CM', '13-Hb', '14-MTT'],
 # }
 find_num = re.compile('[0-9]+')
-
+parser = argparse.ArgumentParser(description='Merge a set of binary masks into a single integer-leveled atlas.')
+parser.add_argument('method', choices=['Numerical', 'Metric'], help='method to combine masks together.  Numerical to use the index when files are of the format "index#-*.nii.gz", higher index numbers take precedence over lower ones where they overlap.  Metric to compute dice overlap and determine a precedence that leads to the minimum change in dice (generally favors smaller ROIs).')
+parser.add_argument('output_path', help='the output atlas NIfTI file.')
+parser.add_argument('labels', metavar='labels', nargs='+', help='a space separated list of binary mask NIfTI files.')
+parser.add_argument('-p', '--processes', nargs='?', default=1, const=None, type=int, help='number of parallel processes to use.  If specified with no value, automatically set to number of CPUs.')
+# parser.add_argument('-v', '--verbose', action='store_true', help='verbose mode')
+# parser.add_argument('-d', '--debug', action='store_true', help='debug mode, interactive prompts')
+# parser.add_argument('-R', '--right', action='store_true', help='segment right thalamus')
 
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print '%s <method: Numerical, Metric> <output_atlas> labels ...' % sys.argv[0]
-        sys.exit(0)
-
-    pool = BetterPool()
-    method = sys.argv[1]
-    out = sys.argv[2]
-    labels = sys.argv[3:]
+    args = parser.parse_args()
+    pool = BetterPool(args.processes)
+    method = args.method
+    out = args.output_path
+    labels = args.labels
     if method == 'Numerical':
         print 'Using the input order'
         method_labels = labels
